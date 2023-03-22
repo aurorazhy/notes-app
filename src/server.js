05adbf5fc07367cb6ce8d84626145b18e89bd32a
Aurora <87532858+aurorazhy@users.noreply.github.com>
2023-03-22 23:17:29 +0700

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+   const server = Hapi.server({
+      port: 24,
+      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+      routes: {
+         cors: {
+            origin: ['*'],
+         },
+      },
+   });
+
+   server.route(routes);
+
+   await server.start();
+   console.log(`Server berjalan link > ${server.info.uri}`);
+};
+
+init();
